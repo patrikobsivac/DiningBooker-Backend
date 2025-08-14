5b21d96a42e2934d8ef050ba9aab8b832be7a60f
Patrik Tovernić Obšivač <patriktovernic@gmail.com>
2025-08-15 00:58:21 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
 const app = express();
 const router = express.Router();
-const PORT = process.env.port || 3000;
+const PORT = 3000;
 
+dotenv.config();
 app.use(express.json());
+app.use(cors());
 app.use("/api", router);
 app.listen(port, () => {
     console.log(`Servis sluša na portu ${PORT}`);
